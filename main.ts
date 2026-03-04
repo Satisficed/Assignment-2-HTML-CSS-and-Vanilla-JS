f88a0091008a151c8956894ba0a394dba495d1ab
Satisficed <342333507@tdsb.ca>
2026-03-04 11:32:01 -0500

    main.ts now has some template code

--- main.ts
+++ main.ts
@@ -8,5 +8,16 @@ form?.addEventListener("submit", (event) => {
     const b: number = Number(formData.get("b"));
     const c: number = Number(formData.get("c"));
     const d: number = Number(formData.get("d"));
+
+    const discriminant = a + b + c + d;
+
+    if (discriminant < 0) {
+        (document.getElementById("result") as HTMLInputElement).value = "No roots";
+    } else if (discriminant > 0) {
+        const rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
+        const rootTwo = (-b - Math.sqrt(discriminant)) / (2 * a);
+    } else {
+        const rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
+    }
 })
 
