6232128dc72f10a96d87036eb1293cfbacebf227
Satisficed <342333507@tdsb.ca>
2026-03-04 10:56:10 -0500

    Adding more from the quadratic formula into main.ts and sytle.css.

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-// import './style.css'
+const form = document.getElementById("cube-form") as HTMLFormElement;
+
+form?.addEventListener("submit", (event) => {
+
+    const formData = new FormData(form);
+
+    const a: number = Number(formData.get("a"));
+    const b: number = Number(formData.get("b"));
+    const c: number = Number(formData.get("c"));
+    const d: number = Number(formData.get("d"));
+})
+
