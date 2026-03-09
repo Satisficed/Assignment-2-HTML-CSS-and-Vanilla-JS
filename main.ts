88c638d5fbd4e30be5c77a2b0c601265d63249a7
Satisficed <342333507@tdsb.ca>
2026-03-09 18:19:41 -0400

    added a table

--- main.ts
+++ main.ts
@@ -6,32 +6,33 @@ form?.addEventListener("submit", (event) => {
     const formData = new FormData(form);
 
     const a: number = Number(formData.get("a"));
-    const b: number = Number(formData.get("b"));
-    const c: number = Number(formData.get("c"));
-    const d: number = Number(formData.get("d"));
-
-    if (a == 0) {
+    
+    if (a === 0) {
         (document.getElementById("result") as HTMLInputElement).value = "Not A Cubic";
-        console.log(d, "Case 0");
+        console.log("Case 0: a-value is zero");
         return;
     }
 
+    const b: number = Number(formData.get("b"));
+    const c: number = Number(formData.get("c"));
+    const d: number = Number(formData.get("d"));
+    
     const p = ((3 * a * c) - b ** 2) / (3 * a ** 2);
     const q = ((27 * a ** 2 * d) - (9 * a * b * c) + (2 * b ** 3)) / (27 * a ** 3)
     const checkRoots = (q / 2) ** 2 + (p / 3) ** 2
     
     if (checkRoots < 0) {
         (document.getElementById("result") as HTMLInputElement).value = "No roots";
-        console.log(d, "Case 1");
+        console.log("Case 1");
     } else if (checkRoots > 0) {d 
         const rootOne = (-b + Math.sqrt(checkRoots)) / (2 * a);
         const rootTwo = (-b - Math.sqrt(checkRoots)) / (2 * a);
         (document.getElementById("result") as HTMLInputElement).value = `x=${rootOne}, x2=${rootTwo}`;
-        console.log(d, "Case 2");
+        console.log("Case 2");
     } else {
         const rootOne = (-b + Math.sqrt(checkRoots)) / (2 * a);
         (document.getElementById("result") as HTMLInputElement).value = `x=${rootOne}`;
-        console.log(d, "Case 3");
+        console.log("Case 3");
     }
 });
 
