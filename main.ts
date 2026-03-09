2692358ee1b5d16b4032a5472b064349117bd34d
Satisficed <342333507@tdsb.ca>
2026-03-08 22:42:16 -0400

    Some progress to searching cubic roots

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 const form = document.getElementById("cube-form") as HTMLFormElement;
 
 form?.addEventListener("submit", (event) => {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault(); // Prevent form submission refresh
 
     const formData = new FormData(form);
 
@@ -10,25 +10,28 @@ form?.addEventListener("submit", (event) => {
     const c: number = Number(formData.get("c"));
     const d: number = Number(formData.get("d"));
 
-    const p = ((3 * a * c) - b ** 2) / (3 * a ** 2);
-    const q = ((27 * a ** 2 * d) - (9 * a * b * c) + (2 * b ** 3)) / (27 * a ** 3)
-    const discriminant = b * b - 4 * a * c;
-    
     if (a == 0) {
         (document.getElementById("result") as HTMLInputElement).value = "Not A Cubic";
         console.log(d, "Case 0");
-    } else if (discriminant < 0) {
+        return;
+    }
+
+    const p = ((3 * a * c) - b ** 2) / (3 * a ** 2);
+    const q = ((27 * a ** 2 * d) - (9 * a * b * c) + (2 * b ** 3)) / (27 * a ** 3)
+    const checkRoots = (q / 2) ** 2 + (p / 3) ** 2
+    
+    if (checkRoots < 0) {
         (document.getElementById("result") as HTMLInputElement).value = "No roots";
         console.log(d, "Case 1");
-    } else if (discriminant > 0) {d 
-        const rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
-        const rootTwo = (-b - Math.sqrt(discriminant)) / (2 * a);
+    } else if (checkRoots > 0) {d 
+        const rootOne = (-b + Math.sqrt(checkRoots)) / (2 * a);
+        const rootTwo = (-b - Math.sqrt(checkRoots)) / (2 * a);
         (document.getElementById("result") as HTMLInputElement).value = `x=${rootOne}, x2=${rootTwo}`;
         console.log(d, "Case 2");
     } else {
-        const rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
+        const rootOne = (-b + Math.sqrt(checkRoots)) / (2 * a);
         (document.getElementById("result") as HTMLInputElement).value = `x=${rootOne}`;
         console.log(d, "Case 3");
     }
-})
+});
 
