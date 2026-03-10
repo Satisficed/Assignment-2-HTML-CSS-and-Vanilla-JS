e9f8f6c6f0222215746956e0b6f5ca14c436f9bd
Satisficed <342333507@tdsb.ca>
2026-03-09 23:05:47 -0400

    Some consts and case element changes.

--- main.ts
+++ main.ts
@@ -17,28 +17,33 @@ form?.addEventListener("submit", (event) => {
     const c: number = Number(formData.get("c"));
     const d: number = Number(formData.get("d"));
     
+    
     const p = ((3 * a * c) - b ** 2) / (3 * a ** 2);
     const q = ((27 * a ** 2 * d) - (9 * a * b * c) + (2 * b ** 3)) / (27 * a ** 3)
-    const checkRoots = (q / 2) ** 2 + (p / 3) ** 2
-    
-    if (checkRoots < 0) {
-        (document.getElementById("result") as HTMLInputElement).value = "No roots";
+    const discriminant = (q / 2) ** 2 + (p / 3) ** 2
+    const h = - b / (3 * a)
+    const y = (-q / 2 + discriminant ** (1/2)) ** (1/3) + (-q / 2 - discriminant ** (1/2)) ** (1/3)
+    const x = y + h
+    if (discriminant < 0) {
+        (document.getElementById("result") as HTMLInputElement).value = `${a}x^3,${b}x^2,${c}x,${d}`;
         console.log("Case 1: 3 Real Roots");
-    } else if (checkRoots > 0) {d 
-        const rootOne = (-b + Math.sqrt(checkRoots)) / (2 * a);
-        const rootTwo = (-b - Math.sqrt(checkRoots)) / (2 * a);
+    } else if (discriminant > 0) {d 
+        const rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
+        const rootTwo = (-b - Math.sqrt(discriminant)) / (2 * a);
         (document.getElementById("result") as HTMLInputElement).value = `x=${rootOne}, x2=${rootTwo}`;
-        console.log("Case 2: 1 Root ");
+        console.log("Case 2: 1 Real Root, 2 Complex Roots");
+        console.log("Testing Values:", q, p, x, y); 
     } else {
         if (p === 0 && q === 0){
-        const rootOne = (-b + Math.sqrt(checkRoots)) / (2 * a);
+        const rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
         (document.getElementById("result") as HTMLInputElement).value = `x=${rootOne}`;
-        console.log("Case 3");  
+        console.log("Case 3: 1 Real Root, Double Roots");  
         } else if (p != 0){
-        const rootOne = (-b + Math.sqrt(checkRoots)) / (2 * a);
+        const rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
         (document.getElementById("result") as HTMLInputElement).value = `x=${rootOne}`;
-        console.log("Case 3");  
+        console.log("Case 4: Triple Roots");  
         }
     }
+   
 });
 
