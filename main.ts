42c7650af9771ef08faa1b91e6f745218d6b9a1f
Satisficed <342333507@tdsb.ca>
2026-03-26 10:28:37 -0400

    Grid Adjusted

--- main.ts
+++ main.ts
@@ -34,41 +34,40 @@ form?.addEventListener("submit", (event) => {
   // Canvas presets
   const ctx = canvas.getContext("2d")!;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
+
+  // 15x15 grid for canvas graph
+  ctx.lineWidth = 0.5;
+  ctx.strokeStyle = "grey";
+  ctx.beginPath();
+  for (let i = 1; i < 30; i++) {
+    ctx.moveTo(i * 20, -100);
+    ctx.lineTo(i * 20, canvas.height);
+    ctx.moveTo(0, i * 20);
+    ctx.lineTo(canvas.width, i * 20);
+  }
+  ctx.stroke();
 
   // x and y axis
+  ctx.strokeStyle = "black";
   ctx.beginPath();
   ctx.moveTo(canvas.width / 2, 0);
   ctx.lineTo(canvas.width / 2, canvas.height);
   ctx.stroke();
   ctx.moveTo(0, canvas.height / 2);
   ctx.lineTo(canvas.width, canvas.height / 2);
-  ctx.lineWidth = 2;
+  ctx.lineWidth = 1.5;
   ctx.stroke();
-  ctx.strokeStyle = "black";
-  // 15x15 grid for canvas graph
-  ctx.lineWidth = 1;
-  for (let i = 1; i < 30; i++) {
-    ctx.strokeStyle = "grey";
-    ctx.beginPath();
-    ctx.moveTo(i * 20, -100);
-    ctx.lineTo(i * 20, canvas.height);
-    ctx.moveTo(0, i * 20);
-    ctx.lineTo(canvas.width, i * 20);
-    ctx.stroke();
-  }
-  
-  ctx.strokeStyle = "crimson";
-  ctx.fillStyle = "crimson";
 
+  ctx.fillStyle = "crimson";
+  ctx.strokeStyle = "crimson";
   // Cases for Cubic Equation
   if (discriminant > 0) {
     console.log("Case 2: 1 Real Root, 2 Complex Roots");
     const u = Math.cbrt(
-      -q / 2 + Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3)),
+      -q / 2 + Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3))
     );
     const v = Math.cbrt(
-      -q / 2 - Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3)),
+      -q / 2 - Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3))
     );
     const x1 = u + v + h;
 
@@ -77,8 +76,9 @@ form?.addEventListener("submit", (event) => {
       .forEach((td) => {
         td.textContent = `Imaginary`;
       });
-    (document.getElementById("x1") as HTMLTableCellElement).textContent =
-      `${x1}`;
+    (
+      document.getElementById("x1") as HTMLTableCellElement
+    ).textContent = `${x1}`;
     (document.getElementById("y1") as HTMLTableCellElement).textContent = `0`;
 
     ctx.beginPath();
@@ -88,7 +88,7 @@ form?.addEventListener("submit", (event) => {
       4,
       13,
       Math.PI * 2,
-      true,
+      true
     );
     ctx.fill();
     ctx.stroke();
@@ -105,20 +105,23 @@ form?.addEventListener("submit", (event) => {
       .forEach((td) => {
         td.textContent = `0`;
       });
-    (document.getElementById("x1") as HTMLTableCellElement).textContent =
-      `${x1}`;
-    (document.getElementById("x2") as HTMLTableCellElement).textContent =
-      `${x2}`;
-    (document.getElementById("x3") as HTMLTableCellElement).textContent =
-      `${x3}`;
+    (
+      document.getElementById("x1") as HTMLTableCellElement
+    ).textContent = `${x1}`;
+    (
+      document.getElementById("x2") as HTMLTableCellElement
+    ).textContent = `${x2}`;
+    (
+      document.getElementById("x3") as HTMLTableCellElement
+    ).textContent = `${x3}`;
   } else {
     if (p === 0 && q === 0) {
       console.log("Case 3: Triple Roots");
       const u = Math.cbrt(
-        -q / 2 + Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3)),
+        -q / 2 + Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3))
       );
       const v = Math.cbrt(
-        -q / 2 - Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3)),
+        -q / 2 - Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3))
       );
       const x1 = u + v + h;
       document
@@ -126,12 +129,15 @@ form?.addEventListener("submit", (event) => {
         .forEach((td) => {
           td.textContent = `0`;
         });
-      (document.getElementById("x1") as HTMLTableCellElement).textContent =
-        `${x1}`;
-      (document.getElementById("x2") as HTMLTableCellElement).textContent =
-        `${x1}`;
-      (document.getElementById("x3") as HTMLTableCellElement).textContent =
-        `${x1}`;
+      (
+        document.getElementById("x1") as HTMLTableCellElement
+      ).textContent = `${x1}`;
+      (
+        document.getElementById("x2") as HTMLTableCellElement
+      ).textContent = `${x1}`;
+      (
+        document.getElementById("x3") as HTMLTableCellElement
+      ).textContent = `${x1}`;
       ctx.beginPath();
       ctx.arc(
         x1 * 15 + canvas.width / 2,
@@ -139,17 +145,17 @@ form?.addEventListener("submit", (event) => {
         4,
         13,
         Math.PI * 2,
-        true,
+        true
       );
       ctx.fill();
       ctx.stroke();
     } else if (p != 0) {
       console.log("Case 4: 1 Real Root, Double Roots");
       const u = Math.cbrt(
-        -q / 2 + Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3)),
+        -q / 2 + Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3))
       );
       const v = Math.cbrt(
-        -q / 2 - Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3)),
+        -q / 2 - Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3))
       );
       const x1 = u + v + h;
       const x2 = Math.cbrt(q / 2) + h;
@@ -158,12 +164,15 @@ form?.addEventListener("submit", (event) => {
         .forEach((td) => {
           td.textContent = `0`;
         });
-      (document.getElementById("x1") as HTMLTableCellElement).textContent =
-        `${x2}`;
-      (document.getElementById("x2") as HTMLTableCellElement).textContent =
-        `${x1}`;
-      (document.getElementById("x3") as HTMLTableCellElement).textContent =
-        `${x1}`;
+      (
+        document.getElementById("x1") as HTMLTableCellElement
+      ).textContent = `${x2}`;
+      (
+        document.getElementById("x2") as HTMLTableCellElement
+      ).textContent = `${x1}`;
+      (
+        document.getElementById("x3") as HTMLTableCellElement
+      ).textContent = `${x1}`;
       ctx.beginPath();
       ctx.arc(
         x1 * 15 + canvas.width / 2,
@@ -171,7 +180,7 @@ form?.addEventListener("submit", (event) => {
         4,
         13,
         Math.PI * 2,
-        true,
+        true
       );
       ctx.fill();
       ctx.moveTo(canvas.width / 2, canvas.height / 2);
@@ -181,16 +190,17 @@ form?.addEventListener("submit", (event) => {
       return;
     }
   }
-  (document.getElementById("result") as HTMLElement).textContent =
-    `${a}x^3 + ${b}x^2 + ${c}x + ${d}`;
+  (
+    document.getElementById("result") as HTMLElement
+  ).textContent = `${a}x^3 + ${b}x^2 + ${c}x + ${d}`;
   (document.getElementById("p") as HTMLTableCellElement).textContent = `${p}`;
   (document.getElementById("q") as HTMLTableCellElement).textContent = `${q}`;
   (
     document.getElementById("Discriminant") as HTMLTableCellElement
   ).textContent = `${discriminant}`;
 
-  ctx.strokeStyle = "lime";
   ctx.beginPath;
+  ctx.strokeStyle = "lime";
   ctx.moveTo(canvas.width / 2, canvas.height / 2);
   ctx.lineTo(20 + canvas.width / 2, 10 + canvas.height / 2);
   ctx.lineTo(30 + canvas.width / 2, 20 + canvas.height / 2);
