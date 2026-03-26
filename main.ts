51888fc3c0e3f9b3bf487979f53242a02ffa169f
Satisficed <342333507@tdsb.ca>
2026-03-25 22:55:43 -0400

    Graph has grids

--- main.ts
+++ main.ts
@@ -30,28 +30,43 @@ form?.addEventListener("submit", (event) => {
   const canvas = document.getElementById("graph") as HTMLCanvasElement;
   if (!canvas) return;
 
-  // make axis 
-  // draw grid
   // put roots as circles
-  // make 
+  // Canvas presets
   const ctx = canvas.getContext("2d")!;
+  ctx.beginPath();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.strokeStyle = "black";
-  ctx.clearRect(0, 0, 800, 500);
+
+  // x and y axis
   ctx.moveTo(canvas.width / 2, 0);
   ctx.lineTo(canvas.width / 2, canvas.height);
   ctx.stroke();
   ctx.moveTo(0, canvas.height / 2);
   ctx.lineTo(canvas.width, canvas.height / 2);
   ctx.stroke();
-  ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
+  // 15x15 grid for canvas graph
+  for (let i = 1; i < 30; i++) {
+    ctx.strokeStyle = "lightgrey";
+    ctx.beginPath();
+    ctx.moveTo(i * 20, -100);
+    ctx.lineTo(i * 20, canvas.height);
+    ctx.moveTo(0, i * 20);
+    ctx.lineTo(canvas.width, i * 20);
+    ctx.stroke();
+  }
+
+  ctx.strokeStyle = "crimson";
+  ctx.fillStyle = "crimson";
+
+  // Cases for Cubic Equation
   if (discriminant > 0) {
     console.log("Case 2: 1 Real Root, 2 Complex Roots");
     const u = Math.cbrt(
-      -q / 2 + Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3))
+      -q / 2 + Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3)),
     );
     const v = Math.cbrt(
-      -q / 2 - Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3))
+      -q / 2 - Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3)),
     );
     const x1 = u + v + h;
     document
@@ -59,10 +74,14 @@ form?.addEventListener("submit", (event) => {
       .forEach((td) => {
         td.textContent = `Imaginary`;
       });
-    (
-      document.getElementById("x1") as HTMLTableCellElement
-    ).textContent = `${x1}`;
+    (document.getElementById("x1") as HTMLTableCellElement).textContent =
+      `${x1}`;
     (document.getElementById("y1") as HTMLTableCellElement).textContent = `0`;
+
+    ctx.beginPath();
+    ctx.arc(100, 75, 4, 13, Math.PI * 2, true);
+    ctx.fill();
+    ctx.stroke();
   } else if (discriminant < 0) {
     console.log("Case 1: 3 Real Roots");
     const k = 2 * Math.sqrt(-p / 3);
@@ -76,23 +95,20 @@ form?.addEventListener("submit", (event) => {
       .forEach((td) => {
         td.textContent = `0`;
       });
-    (
-      document.getElementById("x1") as HTMLTableCellElement
-    ).textContent = `${x1}`;
-    (
-      document.getElementById("x2") as HTMLTableCellElement
-    ).textContent = `${x2}`;
-    (
-      document.getElementById("x3") as HTMLTableCellElement
-    ).textContent = `${x3}`;
+    (document.getElementById("x1") as HTMLTableCellElement).textContent =
+      `${x1}`;
+    (document.getElementById("x2") as HTMLTableCellElement).textContent =
+      `${x2}`;
+    (document.getElementById("x3") as HTMLTableCellElement).textContent =
+      `${x3}`;
   } else {
     if (p === 0 && q === 0) {
       console.log("Case 3: Triple Roots");
       const u = Math.cbrt(
-        -q / 2 + Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3))
+        -q / 2 + Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3)),
       );
       const v = Math.cbrt(
-        -q / 2 - Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3))
+        -q / 2 - Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3)),
       );
       const x1 = u + v + h;
       document
@@ -100,22 +116,19 @@ form?.addEventListener("submit", (event) => {
         .forEach((td) => {
           td.textContent = `0`;
         });
-      (
-        document.getElementById("x1") as HTMLTableCellElement
-      ).textContent = `${x1}`;
-      (
-        document.getElementById("x2") as HTMLTableCellElement
-      ).textContent = `${x1}`;
-      (
-        document.getElementById("x3") as HTMLTableCellElement
-      ).textContent = `${x1}`;
+      (document.getElementById("x1") as HTMLTableCellElement).textContent =
+        `${x1}`;
+      (document.getElementById("x2") as HTMLTableCellElement).textContent =
+        `${x1}`;
+      (document.getElementById("x3") as HTMLTableCellElement).textContent =
+        `${x1}`;
     } else if (p != 0) {
       console.log("Case 4: 1 Real Root, Double Roots");
       const u = Math.cbrt(
-        -q / 2 + Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3))
+        -q / 2 + Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3)),
       );
       const v = Math.cbrt(
-        -q / 2 - Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3))
+        -q / 2 - Math.sqrt(Math.abs((q / 2) ** 2 + (p / 3) ** 3)),
       );
       const x1 = u + v + h;
       const x2 = Math.cbrt(q / 2) + h;
@@ -124,47 +137,30 @@ form?.addEventListener("submit", (event) => {
         .forEach((td) => {
           td.textContent = `0`;
         });
-      (
-        document.getElementById("x1") as HTMLTableCellElement
-      ).textContent = `${x2}`;
-      (
-        document.getElementById("x2") as HTMLTableCellElement
-      ).textContent = `${x1}`;
-      (
-        document.getElementById("x3") as HTMLTableCellElement
-      ).textContent = `${x1}`;
+      (document.getElementById("x1") as HTMLTableCellElement).textContent =
+        `${x2}`;
+      (document.getElementById("x2") as HTMLTableCellElement).textContent =
+        `${x1}`;
+      (document.getElementById("x3") as HTMLTableCellElement).textContent =
+        `${x1}`;
     } else {
       console.log("Case 5: Unexpected Result");
       return;
     }
   }
-  (
-    document.getElementById("result") as HTMLElement
-  ).textContent = `${a}x^3 + ${b}x^2 + ${c}x + ${d}`;
+  (document.getElementById("result") as HTMLElement).textContent =
+    `${a}x^3 + ${b}x^2 + ${c}x + ${d}`;
   (document.getElementById("p") as HTMLTableCellElement).textContent = `${p}`;
   (document.getElementById("q") as HTMLTableCellElement).textContent = `${q}`;
   (
     document.getElementById("Discriminant") as HTMLTableCellElement
   ).textContent = `${discriminant}`;
 
-  for (let i = 10; i < 60; i++) { 
-    ctx.strokeStyle = "blue";
-    ctx.beginPath();
-    ctx.moveTo(i * 20, -100);
-    ctx.lineTo(i * 20, canvas.height);
-    ctx.moveTo(0, i * 20);
-    ctx.lineTo(canvas.width, i * 20);
-    ctx.stroke();
-  }
-  
-  ctx.strokeStyle = "red";
-  ctx.beginPath();
+  ctx.strokeStyle = "navyBlue";
+  ctx.beginPath;
   ctx.moveTo(canvas.width / 2, canvas.height / 2);
   ctx.lineTo(20 + canvas.width / 2, 10 + canvas.height / 2);
-  ctx.arc(75, 75, 50, 0, Math.PI * 2, true);
-  ctx.fill
   ctx.lineTo(30 + canvas.width / 2, 20 + canvas.height / 2);
   ctx.lineTo(40 + canvas.width / 2, 30 + canvas.height / 2);
   ctx.stroke();
-  
 });
